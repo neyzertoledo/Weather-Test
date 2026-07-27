a599863e33e4b0e6679ce60eedaaa3a9c1e3c1ed
Neyzer Toledo <neyzer.toledo@dexcom.com>
2026-07-27 11:14:38 -0700

    Initial Commit

---
+++ Weather Test/Weather_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_TestApp.swift
+//  Weather Test
+//
+//  Created by Neyzer Toledo on 27/07/26.
+//
+
+import SwiftUI
+
+@main
+struct Weather_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
