b2c1b4dee2143d4fb3fa889b647917fbb45b9a77
Neyzer Toledo <neyzer.toledo@dexcom.com>
2026-08-11 10:59:52 -0700

    location permissions

---
+++ Weather Test/Services/LocationService.swift
@@ -0,0 +1,13 @@
+//
+//  LocationService.swift
+//  Weather Test
+//
+//  Created by Neyzer Toledo on 11/08/26.
+//
+
+import Foundation
+import CoreLocation
+
+@Observable
+class LocationService: NSObject, CLLocationManagerDelegate {
+}
